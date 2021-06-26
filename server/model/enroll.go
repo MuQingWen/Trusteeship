8dc332228ef418a2c81eb8f4079f9b36820eadcf
MuQingWen <61997336+MuQingWen@users.noreply.github.com>
2021-06-26 14:52:32 +0800

    final

--- server/model/enroll.go
+++ server/model/enroll.go
@@ -1,4 +1,3 @@
-// 自动生成模板Enroll
 package model
 
 import (
@@ -7,20 +6,18 @@ import (
 
 // 如果含有time.Time 请自行import time包
 type Enroll struct {
-      global.GVA_MODEL
-      Cno  string `json:"cno" form:"cno" gorm:"column:cno;comment:班级序号;type:varchar(255);"`
-      Cname  string `json:"cname" form:"cname" gorm:"column:cname;comment:班级名称;type:varchar(255);"`
-      Ccycle  string `json:"ccycle" form:"ccycle" gorm:"column:ccycle;comment:班级周期;type:varchar(255);"`
-      Tname  string `json:"tname" form:"tname" gorm:"column:tname;comment:教师序号;type:varchar(255);"`
-      Ctime  string `json:"ctime" form:"ctime" gorm:"column:ctime;comment:上课时间;type:varchar(255);"`
-      Caccount  int `json:"caccount" form:"caccount" gorm:"column:caccount;comment:班级学费;type:int;size:10;"`
-      Sname  string `json:"sname" form:"sname" gorm:"column:sname;comment:学生姓名;type:varchar(255);"`
-      Saccount  string `json:"saccount" form:"saccount" gorm:"column:saccount;comment:学生缴费;type:varchar(255);"`
-      Sphone  string `json:"sphone" form:"sphone" gorm:"column:sphone;comment:学生电话;type:varchar(255);"`
+	global.GVA_MODEL
+	Cno      string `json:"cno" form:"cno" gorm:"column:cno;comment:班级序号;type:varchar(255);"`
+	Cname    string `json:"cname" form:"cname" gorm:"column:cname;comment:班级名称;type:varchar(255);"`
+	Ccycle   string `json:"ccycle" form:"ccycle" gorm:"column:ccycle;comment:班级周期;type:varchar(255);"`
+	Tname    string `json:"tname" form:"tname" gorm:"column:tname;comment:教师序号;type:varchar(255);"`
+	Ctime    string `json:"ctime" form:"ctime" gorm:"column:ctime;comment:上课时间;type:varchar(255);"`
+	Caccount int    `json:"caccount" form:"caccount" gorm:"column:caccount;comment:班级学费;type:int;size:10;"`
+	Sname    string `json:"sname" form:"sname" gorm:"column:sname;comment:学生姓名;type:varchar(255);"`
+	Saccount string `json:"saccount" form:"saccount" gorm:"column:saccount;comment:学生缴费;type:varchar(255);"`
+	Sphone   string `json:"sphone" form:"sphone" gorm:"column:sphone;comment:学生电话;type:varchar(255);"`
 }
-
 
 func (Enroll) TableName() string {
-  return "enroll"
+	return "enroll"
 }
-
--- server/model/schedule.go
+++ server/model/schedule.go
@@ -1,4 +1,3 @@
-// 自动生成模板Schedule
 package model
 
 import (
@@ -7,16 +6,14 @@ import (
 
 // 如果含有time.Time 请自行import time包
 type Schedule struct {
-      global.GVA_MODEL
-      Cno  string `json:"cno" form:"cno" gorm:"column:cno;comment:班级序号;type:varchar(255);"`
-      Cname  string `json:"cname" form:"cname" gorm:"column:cname;comment:班级科目;type:varchar(255);"`
-      Tno  string `json:"tno" form:"tno" gorm:"column:tno;comment:教师序号;type:varchar(255);"`
-      Sname  string `json:"sname" form:"sname" gorm:"column:sname;comment:学生姓名;type:varchar(255);"`
-      Ctime  string `json:"ctime" form:"ctime" gorm:"column:ctime;comment:上课时间;type:varchar(255);"`
+	global.GVA_MODEL
+	Cno   string `json:"cno" form:"cno" gorm:"column:cno;comment:班级序号;type:varchar(255);"`
+	Cname string `json:"cname" form:"cname" gorm:"column:cname;comment:班级科目;type:varchar(255);"`
+	Tno   string `json:"tno" form:"tno" gorm:"column:tno;comment:教师序号;type:varchar(255);"`
+	Sname string `json:"sname" form:"sname" gorm:"column:sname;comment:学生姓名;type:varchar(255);"`
+	Ctime string `json:"ctime" form:"ctime" gorm:"column:ctime;comment:上课时间;type:varchar(255);"`
 }
-
 
 func (Schedule) TableName() string {
-  return "schedule"
+	return "schedule"
 }
-
